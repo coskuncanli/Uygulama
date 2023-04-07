e0d61e6e21660459644275da9408d26d90fe10ce
Coşkun Canlı <coskun.canli@gmail.com>
2023-04-07 15:46:54 +0300

    mlksm

---
+++ Desktop/emin/SinavUygulama/SinavUygulama/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SinavUygulama.Models;
+using System.Diagnostics;
+
+namespace SinavUygulama.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Kayit(Uye uye)
+        {
+            return Content($"Adı: {uye.Ad}, Soyadı: {uye.Soyad}, Doğum Tarihi: {uye.DogumTarihi}, Adresi: {uye.Adres} ");
+        }
+
+        public IActionResult Islem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Islem(double s1, double s2, int islem)
+        {
+            double sonuc = 0;
+            switch (islem)
+            {
+                case 0: sonuc = s1 + s2; break;
+                case 1: sonuc = s1 - s2; break;
+                case 2: sonuc = s1 * s2; break;
+                case 3: sonuc = s1 / s2; break;
+            }
+
+            return Content($"İşlem Sonucu: {sonuc}");
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Desktop/emin/SinavUygulama/SinavUygulama/Models/Uye.cs
@@ -0,0 +1,10 @@
+﻿namespace SinavUygulama.Models
+{
+    public class Uye
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public string Adres { get; set; }
+    }
+}
